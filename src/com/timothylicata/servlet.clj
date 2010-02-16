39464ec6766bd76680a8d2865472b4f09207a53c
Tim Licata <tlicata@hotpads.com>
2010-02-16 00:08:49 -0500

    add nav links and some css

--- src/com/timothylicata/servlet.clj
+++ src/com/timothylicata/servlet.clj
@@ -8,15 +8,18 @@
   (html
     [:html
      [:head
-      [:title "Tim's Online World"]]
-;;    (include-css "/css/main.css")]
+      [:title "Tim's Online World"]
+      (include-css "/css/main.css")]
 ;;    (include-js "/js/combined.js")]
      [:body
       [:div#page
        [:div#header [:h1 (link-to "http://www.timothylicata.com" "Tim's Online World")]]
        [:div#nav
         [:ul
-         [:li (link-to "about.html" "About")]]]
+         [:li (link-to "about.html" "About")]
+         [:li (link-to "contact.html" "Contact")]
+         [:li (link-to "faq.html" "FAQ")]
+         [:li (link-to "work.html" "Work")]]]
        [:div#body
         [:p "Index prease"]]
        [:div#footer
