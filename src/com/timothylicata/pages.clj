7212a9c993232c4e25b39de266fa73837e06c712
Tim Licata <tlicata@hotpads.com>
2010-02-21 19:17:36 -0500

    add wikipedia link

--- src/com/timothylicata/pages.clj
+++ src/com/timothylicata/pages.clj
@@ -43,7 +43,8 @@
       (nav)
       [:div#body
        [:h3 "Manifesto"]
-       [:p "Manifestos are cool.  Everyone knows that.  According to Wikipedia,"]
+       [:p "Manifestos are cool.  Everyone knows that.  According to "
+        (link-to "http://en.wikipedia.org/wiki/Manifesto" "Wikipedia") ","]
        [:blockquote "A manifesto is a public declaration of principles and intentions, often political in nature. However, manifestos relating to religious belief are generally referred to as a creed. Manifestos may also be life stance-related."]
        [:p "I think I'm after the 'Life Stance' part.  I don't have a concrete one yet but I like the idea so I'm hoping having this here will force me to write one."]
        [:h3 "FAQ"]
