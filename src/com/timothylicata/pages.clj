e193f79e0968ce5d7949816075d18150a4674c86
Tim Licata <tlicata@hotpads.com>
2010-02-26 01:24:04 -0500

    link manifesto to wikipedia examples

--- src/com/timothylicata/pages.clj
+++ src/com/timothylicata/pages.clj
@@ -51,7 +51,20 @@
         [:p "Manifestos are cool.  Everyone knows that.  According to "
          (link-to "http://en.wikipedia.org/wiki/Manifesto" "Wikipedia") ","]
         [:blockquote "A manifesto is a public declaration of principles and intentions, often political in nature. However, manifestos relating to religious belief are generally referred to as a creed. Manifestos may also be life stance-related."]
-        [:p "I think I'm after the 'Life Stance' part.  I don't have a concrete one yet but I like the idea so I'm hoping that leaving this space here will force me to write one."]]
+        [:p "I think I'm after the "
+         (link-to "http://en.wikipedia.org/wiki/Life_stance" "Life Stance")
+         " part.  Let's take a look at some of the notable manifestos listed."]
+        [:ul
+         [:li (link-to "http://en.wikipedia.org/wiki/United_States_Declaration_of_Independence" "United States Declaration of Independence")]
+         [:li (link-to "http://en.wikipedia.org/wiki/The_Communist_Manifesto" "The Communist Manifesto")]
+         [:li (link-to "http://en.wikisource.org/wiki/Industrial_Society_and_Its_Future" "Industrial Society and Its Future")]
+         [:li (link-to "http://en.wikipedia.org/wiki/The_Cathedral_and_the_Bazaar" "The Cathedral and the Bazaar")]
+         [:li (link-to "http://www.phrack.org/issues.html?issue=7&id=3&mode=txt" "The Hacker's Manifesto")]
+         [:li (link-to "http://en.wikipedia.org/wiki/Agile_Manifesto" "Agile Manifesto (?)")]]
+        [:p "Most of these seem to be extreme in both their viewpoints and their word counts.  That's not really my style.  I believe it was Ferris Bueller who said:"]
+         [:blockquote "Isms in my opinion are not good. A person should not believe in an ism, he should believe in himself. I quote John Lennon:"
+          [:blockquote "I don't believe in Beatles, I just believe in me."] "Good point there.  After all, he was the Walrus."]
+        [:p "So that's me.  I don't believe in an Isms, I just believe in myself and, of course, my 'Life Stance'.  That I haven't written yet."]]
        [:div.blurb
         [:h3 "FAQ"]
         [:div.question
