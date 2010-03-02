00c9f44a29c72898b97b72e73645736dba61012e
Tim Licata <tlicata@hotpads.com>
2010-03-02 00:26:59 -0500

    yui-grid layout

--- src/com/timothylicata/pages.clj
+++ src/com/timothylicata/pages.clj
@@ -3,12 +3,10 @@
 
 (defn css
   []
-  (let [global-styles (list "/css/main.css" "http://yui.yahooapis.com/2.8.0r4/build/fonts/fonts-min.css")]
+  (let [global-styles (list
+    "http://yui.yahooapis.com/2.8.0r4/build/reset-fonts-grids/reset-fonts-grids.css"
+    "/css/main.css")]
     (apply include-css global-styles)))
-
-(defn header
-  [request]
-  [:div#header [:h1 (link-to "/" "Tim's Online World")]])
 
 (defn nav
   [request]
@@ -28,49 +26,71 @@
         [:li (link-to "contact.html" "Contact")]
         [:li (link-to (. user-service createLoginURL (:uri request)) "Log In")]])]))
 
+(defn header
+  [request]
+  [:div#hd
+   [:h1 (link-to "/" "Tim's Online World")]
+   (nav request)])
+
+(defn sidebar [request]
+  [:div#sidebar
+   [:div.blurb
+    [:h1 "A few of my favorite things."]
+    [:ul
+     [:li "Paul Graham link or two"]
+     [:li "JavaScript link or two"]
+     [:li "Clojure link or two"]
+     [:li "HotPads.com"]
+     [:li "TheSixtyOne.com"]]]])
+
 (defn footer [request]
-  [:div#footer
+  [:div#ft
    [:p "Powered by "
     (link-to "http://code.google.com/appengine/" "Google App Engine") ", "
     (link-to "http://clojure.org" "Clojure") " and  "
     (link-to "http://github.com/weavejester/compojure" "Compojure")]])
 
-(defn home-page
-  [request]
+(defn page
+  [request info]
   (html
     [:html
-     [:head [:title "Tim's Online World"] (css)]
+     [:head [:title (:title info)] (css)]
      [:body
-      (header request)
-      (nav request)
-      [:div#body
-       [:div.blurb
-        [:ul
-         [:li "server-port: " (:server-port request)]
-         [:li "server-name: " (:server-name request)]
-         [:li "remote-addr: " (:remote-addr request)]
-         [:li "uri: " (:uri request)]
-         [:li "query-string: " (:query-string request)]
-         [:li "scheme: " (:scheme request)]
-         [:li "request-method: " (:request-method request)]
-         [:li "headers: " (:headers request)]
-         [:li "content-type: " (:content-type request)]
-         [:li "character-encoding: " (:character-encoding request)]
-         [:li "body: " (:body request)]
-         [:li "appengine/user-info: " (:appengine/user-info request)]]]]
-      (footer request)]]))
+      [:div#doc2.yui-t5
+       (header request)
+       [:div#bd
+        [:div#yui-main [:div.yui-b (:body info)]]
+        [:div.yui-b (sidebar request)]]
+       (footer request)]]]))
+
+(defn home-page
+  [request]
+  (page request
+    {:title "Tim's Online World"
+     :body
+     [:div.blurb
+      [:ul
+       [:li "server-port: " (:server-port request)]
+       [:li "server-name: " (:server-name request)]
+       [:li "remote-addr: " (:remote-addr request)]
+       [:li "uri: " (:uri request)]
+       [:li "query-string: " (:query-string request)]
+       [:li "scheme: " (:scheme request)]
+       [:li "request-method: " (:request-method request)]
+       [:li "headers: " (:headers request)]
+       [:li "content-type: " (:content-type request)]
+       [:li "character-encoding: " (:character-encoding request)]
+       [:li "body: " (:body request)]
+       [:li "appengine/user-info: " (:appengine/user-info request)]]]}))
 
 (defn about
   [request]
-  (html
-    [:html
-     [:head [:title "About Tim"] (css)]
-     [:body
-      (header request)
-      (nav request)
-      [:div#body
+  (page request
+     {:title "About Tim"
+      :body
+      [:div
        [:div.blurb
-        [:h3 "Manifesto"]
+        [:h1 "Manifesto"]
         [:p "Manifestos are cool.  Everyone knows that.  According to "
          (link-to "http://en.wikipedia.org/wiki/Manifesto" "Wikipedia") ","]
         [:blockquote "A manifesto is a public declaration of principles and intentions, often political in nature. However, manifestos relating to religious belief are generally referred to as a creed. Manifestos may also be life stance-related."]
@@ -89,45 +109,38 @@
           [:blockquote "I don't believe in Beatles, I just believe in me."] "Good point there.  After all, he was the Walrus."]
         [:p "So that's me.  I don't believe in an Isms, I just believe in myself and, of course, my Life Stance.  That I haven't written yet."]]
        [:div.blurb
-        [:h3 "FAQ"]
+        [:h1 "FAQ"]
         [:div.question
          [:p "Do you want to make more money?"]]
         [:div.answer
-         [:p "Sure, we all do."]]]]
-      (footer request)]]))
+         [:p "Sure, we all do."]]]]}))
 
 (defn contact
   [request]
-  (html
-    [:html
-     [:head [:title "Contact Tim"] (css)]
-     [:body
-      (header request)
-      (nav request)
-      [:div
-       [:div.blurb
-        [:h3 "Email"]
-        [:p "The best way to get ahold of me is through email.  tim [at] [this domain]."]]]
-      (footer request)]]))
+  (page
+    request
+    {:title "Contact Tim"
+     :body
+     [:div
+      [:div.blurb
+       [:h1 "Email"]
+       [:p "The best way to get ahold of me is through email.  tim [at] [this domain]."]]]}))
 
 (defn work
   [request]
-  (html
-    [:html
-     [:head [:title "Tim's Work"] (css)]
-     [:body
-      (header request)
-      (nav request)
+  (page
+    request
+    {:title "Tim's Work"
+     :body
       [:div
        [:div.blurb
-        [:h3 "HotPads.com"]
+        [:h1 "HotPads.com"]
         [:p "My first job"]]
        [:div.blurb
-        [:h3 "Synacor, Inc."]
+        [:h1 "Synacor, Inc."]
         [:p "My second job"]]
        [:div.blurb
-        [:h3 "This Site"]
+        [:h1 "This Site"]
         [:p "One of the reasons I built this site was to give myself a programming playground.  This site is hosted on Google App Engine.  It is written in Clojure, which is a really cool functional language that runs on the JVM.  Luckily, someone wrote a very nice web application library for Clojure called Compojure.  On the client side I plan on refining my HTML/CSS/JS.  My JavaScript library of choice is JQuery.  And sometimes I add a Flash or Flex app."]]
        [:div.blurb
-        [:h3 "Resume"]]]
-      (footer request)]]))
+        [:h1 "Resume"]]]}))
