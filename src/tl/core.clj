cbbc36e8648efd7576e2df5a4ecf116b0f115bb5
Tim Licata <tlicata@hotpads.com>
2010-07-23 22:16:43 -0400

    main nav links to working pages

--- src/tl/core.clj
+++ src/tl/core.clj
@@ -1,11 +1,17 @@
 (ns tl.core
   (:use compojure.core
 	ring.adapter.jetty
-	tl.pages.home)
+	tl.pages.golf
+	tl.pages.home
+	tl.pages.programming
+	tl.pages.youtubes)
   (:require [compojure.route :as route]))
 
 (defroutes tl
   (GET "/" [] (home-page {}))
+  (GET "/golf.html" [] (golf {}))
+  (GET "/programming.html" [] (programming {}))
+  (GET "/youtubes.html" [] (youtubes {}))
   (route/files "/" {:root "war/public"})
   (ANY "*" [] {:status 404 :body "<h1>404</h1>"}))
 
