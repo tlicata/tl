54bc8cecbc0288ef08163596606013caf2eb89b8
Tim Licata <tlicata@hotpads.com>
2010-09-08 00:09:07 -0400

    center polymap on buffalo.  add hash to url bar. fiddle with zooms.

--- war/public/js/poly.js
+++ war/public/js/poly.js
@@ -9,11 +9,12 @@ tl.polymaps = (function () {
 	var insertMap = function () {
 		var container = $("#polymaps-container")[0];
 		var map = po.map()
-			.container(container.appendChild(po.svg("svg")))
-			.center({lat: 40, lon: 0})
-			.zoomRange([1, 4])
-			.zoom(2)
-			.add(po.interact());
+		.container(container.appendChild(po.svg("svg")))
+		.center({lat: 42.938, lon: -78.986})
+		.zoom(8)
+		.zoomRange([2, 9])
+		.add(po.hash())
+		.add(po.interact());
 
 		map.add(po.image()
 				.url("http://s3.amazonaws.com/com.modestmaps.bluemarble/{Z}-r{Y}-c{X}.jpg"));
