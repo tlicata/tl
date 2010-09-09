2d37ca8850b7208197edae8cfeb3d978c1c98b45
Tim Licata <tlicata@hotpads.com>
2010-09-09 10:31:15 -0400

    back to route/files since wrap-file wasn't working

--- src/tl/servlet.clj
+++ src/tl/servlet.clj
@@ -2,7 +2,6 @@
   (:gen-class :extends javax.servlet.http.HttpServlet)
   (:use
    [compojure.core :only [defroutes GET POST]]
-   [ring.middleware.file :only [wrap-file]]
 	[ring.util.servlet :only [defservice]]
 	tl.pages.golf
 	tl.pages.programming
@@ -15,8 +14,7 @@
   (POST "/golf.html" [name] (golf-post name))
   (GET "/programming.html" [] (programming {}))
   (GET "/youtubes.html" [] (youtubes {}))
+  (route/files "/" {:root "./war/public"})
   (route/not-found "Not Found"))
 
-(defservice
-  (-> tl
-	  (wrap-file "public")))
+(defservice tl)
