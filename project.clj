c139e0d7800c1a76053be1a56a03da1565fb6136
Tim Licata <tlicata@hotpads.com>
2010-09-10 05:12:41 -0400

    upgrade clojure and clojure-contrib to 1.2

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.3.1"]
                  [compojure "0.4.1"]
                  [hiccup "0.2.6"]
-                 [org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+                 [org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [ring "0.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :compile-path "war/WEB-INF/classes/"
