53e24a9365fd7e90f6918df7d7ad700d11bb8aef
Tim Licata <tlicata@hotpads.com>
2010-09-14 15:48:39 -0400

    split up routes

--- src/tl/servlet.clj
+++ src/tl/servlet.clj
@@ -7,13 +7,25 @@
    [tl.pages.programming :only [map-page]])
   (:require [compojure.route :as route]))
 
-(defroutes tl
-  (GET "/" [] (redirect "/maps/"))
+(defroutes tl-routes
+  (GET "/" [] (redirect "/maps/")))
+
+(defroutes map-routes
   (GET "/maps/" request (map-page request))
   (GET "/maps/:kind" request (map-page request))
-  (GET "/maps/fullscreen/:kind" request (map-page request))
+  (GET "/maps/fullscreen/:kind" request (map-page request)))
+
+(defroutes static-routes
   (route/files "/" {:root "./war/public"})
-  (route/files "/" {:root "./public"})
+  (route/files "/" {:root "./public"}))
+
+(defroutes error-routes
   (route/not-found "Not Found"))
 
-(defservice tl)
+(defroutes all-routes
+  tl-routes
+  map-routes
+  static-routes
+  error-routes)
+
+(defservice all-routes)
