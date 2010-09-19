297c57ab4d19541e1a00ed37d9a564f25c74ce0a
Tim Licata <tlicata@hotpads.com>
2010-09-18 23:08:53 -0400

    modify location

--- src/tl/pages/home.clj
+++ src/tl/pages/home.clj
@@ -3,10 +3,10 @@
 		[tl.pages.global :only [page]]))
 
 (def welcome-blurb [:p (str-join "  -  " ["Tim Licata"
-										  "Lockport, NY"
 										  "Programmer"
 										  "Golfer"
-										  "Cool Guy"])])
+										  "Cool Guy"
+										  "Lockport, NY & Washingon, DC"])])
 
 (defn home-page [request]
   (page {:title "Home"
