cb60c1e129ee77160e4d6d07fc9e0821963c3ef1
Tim Licata <tlicata@hotpads.com>
2010-12-13 20:40:34 -0500

    upgrade compojure.  need :keep-non-project-classes.
    for :keep-non-project-classes, see
    http://groups.google.com/group/clojure/browse_frm/thread/6ebaf57016766791#
    http://dev.clojure.org/jira/browse/CLJ-322

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject tl "0.1.0-SNAPSHOT"
   :description "My personal site written in Clojure"
-  :dependencies [[compojure "0.4.1"]
+  :dependencies [[compojure "0.5.3"]
                  [hiccup "0.2.6"]
                  [org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring "0.3.0-RC2"]]
   :dev-dependencies [[appengine-magic "0.4.0-SNAPSHOT"]
 					 [swank-clojure "1.2.1"]]
-  :aot [tl.app_servlet])
+  :aot [tl.app_servlet]
+  :keep-non-project-classes true)
 
 
