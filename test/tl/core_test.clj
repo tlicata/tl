41aadf49fb3819a65c51e1be3adaec930b3480dd
Tim Licata <tlicata@hotpads.com>
2011-01-01 15:41:55 -0500

    add some tests for routes.
    use ring-mock for generating test request objects. also used
    appengine-magic to create GAE's local services as fixtures.

--- test/tl/core_test.clj
+++ test/tl/core_test.clj
@@ -1,6 +1,31 @@
 (ns tl.core-test
   (:use [tl.core] :reload-all)
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use [ring.mock.request])
+  (:require [appengine-magic.testing :as ae-testing]))
 
-(deftest replace-me ;; FIXME: write
-  (is false))
+(def valid-routes ["/"
+				   "/contact/"
+				   "/login/"
+				   "/maps/"
+				   "/maps/google"
+				   "/maps/hotpads"
+				   "/maps/polymaps"])
+
+(def invalid-routes ["/garbage"
+					 "/garbage/"
+					 "/maps/garbage"
+					 "/maps/garbage/"])
+
+(use-fixtures :each (ae-testing/local-services :all))
+
+(deftest test-routes
+  (doseq [route valid-routes]
+	(let [response (all-routes (request :get route))]
+	  (is (= (:status response) 200) route))))
+
+(deftest test-invalid-routes
+  (doseq [route invalid-routes]
+	(let [response (all-routes (request :get route))]
+	  (is (= (:status response) 404) route))))
+		 
