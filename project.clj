c6b13e347d7fc2235fd119598c4669e01b0290cd
Tim Licata <tlicata@hotpads.com>
2011-01-01 15:35:48 -0500

    add ring-mock dependency for testing routes

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring "0.3.0-RC2"]]
   :dev-dependencies [[appengine-magic "0.4.0-SNAPSHOT"]
+					 [ring-mock "0.1.1"]
 					 [swank-clojure "1.2.1"]]
   :aot [tl.app_servlet]
   :keep-non-project-classes true)
