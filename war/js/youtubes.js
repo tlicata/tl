ed4802f7411f47f6063adb34fa4dd2e0b6beb7eb
Tim Licata <tlicata@hotpads.com>
2011-03-12 23:56:37 -0500

    stick with fixed height player for now.
    until i can figure out cross browser css.

--- war/js/youtubes.js
+++ war/js/youtubes.js
@@ -9,7 +9,7 @@ tl.youtubes = (function () {
 	return {
 		play: function (video, autoplay, loopMode) {
 			$(function () {
-				swfobject.embedSWF(video, "swf-div", "100%", "100%", "9", null,
+				swfobject.embedSWF(video, "swf-div", "100%", "290px", "9", null,
 								   {autoplay: autoplay, loop: loopMode});
 			});
 		}
