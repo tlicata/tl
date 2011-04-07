ff34bed8aada279fa95ee2042b1dfbfded174749
Tim Licata <tlicata@hotpads.com>
2011-04-06 20:35:33 -0400

    appease jslint on gmap.js

--- war/js/gmap.js
+++ war/js/gmap.js
@@ -1,15 +1,21 @@
-(function () {
+var google, tl;
+
+if (!tl) {
+	tl = {};
+}
+
+tl.gmap = (function () {
 
 	var init = function () {
-		var latlng = new google.maps.LatLng(37.760401,-122.427521);
+		var latlng = new google.maps.LatLng(37.760401, -122.427521);
 		var options = {
 			zoom: 11,
 			center: latlng,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map(document.getElementById("gmap"), options);
-	}
+	};
 
 	$(document).ready(init);
 
-})();
+}());
