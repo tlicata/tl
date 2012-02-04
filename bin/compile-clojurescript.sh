b1a5ea3cbd3150c8fa28b5b9f5d04804b7cce8cb
Tim Licata <tlicata@hotpads.com>
2012-02-04 10:01:22 -0800

    remove compile-clojurescript hook.
    going to use lein-cljsbuild?

--- bin/compile-clojurescript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf war/js/bin/out
-mkdir -p war/js/bin/out
-cljsc src/tl/cljs > war/js/bin/all.js
-mv out war/js/bin/
--- project.clj
+++ project.clj
@@ -1,7 +1,3 @@
-(require '[robert.hooke :as hooke])
-(require '[lancet :as lancet])
-(require '[leiningen.appengine-prepare :as appengine-prepare])
-
 (defproject tl "0.1.0-SNAPSHOT"
   :description "My personal site written in Clojure"
   :dependencies [[compojure "1.0.0"]
@@ -12,11 +8,3 @@
                  [ring/ring-jetty-adapter "0.3.9"]
                  [scriptjure "0.1.24"]]
   :dev-dependencies [[ring-mock "0.1.1"]])
-
-(defn compile-clojurescript
-  [task project & args]
-  (task project)
-  (println "Compiling clojurescript")
-  (lancet/exec {:executable "./bin/compile-clojurescript.sh"}))
-
-(hooke/add-hook #'appengine-prepare/appengine-prepare compile-clojurescript)
