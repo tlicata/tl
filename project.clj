b8b1fd2f3a3286b6c48b440e2b4cfcbe10c5e705
Tim Licata <tlicata@hotpads.com>
2012-02-04 12:57:36 -0800

    lein-cljsbuild as dev-dependency

--- project.clj
+++ project.clj
@@ -7,4 +7,9 @@
                  [ring "1.0.1"]
                  [ring/ring-jetty-adapter "0.3.9"]
                  [scriptjure "0.1.24"]]
-  :dev-dependencies [[ring-mock "0.1.1"]])
+  :dev-dependencies [[lein-cljsbuild "0.0.11"]
+                     [ring-mock "0.1.1"]]
+  :cljsbuild {:source-path "src-cljs/tl"
+              :compiler {:output-to "war/js/bin/all.js"
+                         :optimizations :advanced}}
+  :hooks [leiningen.cljsbuild])
