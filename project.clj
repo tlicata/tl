48aac0845ff83544238c4b2d9b27d643259c427e
Tim Licata <tlicata@hotpads.com>
2012-02-25 10:27:04 -0800

    upgrade to lein 1.7 and cljsbuild 0.1.0

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[clj-redis "0.0.12"]
                  [compojure "1.0.0"]
                  [hiccup "0.3.8"]
-                 [lein-cljsbuild "0.0.14"]
                  [noir "1.2.2"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
@@ -11,13 +10,16 @@
                  [ring/ring-jetty-adapter "0.3.9"]
                  [scriptjure "0.1.24"]]
   :dev-dependencies [[ring-mock "0.1.1"]]
+  :plugins [[lein-cljsbuild "0.1.0"]]
   :aot [tl.core]
-  :cljsbuild [{:source-path "src-cljs"
-               :compiler {:output-to "resources/public/js/bin/all.min.js"
-                          :optimizations :advanced
-                          :pretty-print false}}
-              {:source-path "src-cljs"
-               :compiler {:output-to "resources/public/js/bin/all.js"
-                          :optimizations :simple
-                          :pretty-print true}}]
+  :cljsbuild
+  {:builds [{:source-path "src-cljs",
+             :compiler {:output-to "resources/public/js/bin/all.min.js",
+                        :optimizations :advanced,
+                        :pretty-print false}}
+            {:source-path "src-cljs",
+             :compiler
+             {:output-to "resources/public/js/bin/all.js",
+              :optimizations :simple,
+              :pretty-print true}}]}
   :hooks [leiningen.cljsbuild])
