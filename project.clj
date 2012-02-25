26f7fa12d5ee1a1ecb565448437c6e934ef649a6
Tim Licata <tlicata@hotpads.com>
2012-02-24 19:39:25 -0800

    lein-cljsbuild is dependency, not dev-dependency

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :dependencies [[clj-redis "0.0.12"]
                  [compojure "1.0.0"]
                  [hiccup "0.3.8"]
+                 [lein-cljsbuild "0.0.11"]
                  [noir "1.2.2"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [ring "1.0.1"]
                  [ring/ring-jetty-adapter "0.3.9"]
                  [scriptjure "0.1.24"]]
-  :dev-dependencies [[lein-cljsbuild "0.0.11"]
-                     [ring-mock "0.1.1"]]
+  :dev-dependencies [[ring-mock "0.1.1"]]
   :aot [tl.core]
   :cljsbuild [{:source-path "src-cljs"
                :compiler {:output-to "resources/public/js/bin/all.min.js"
