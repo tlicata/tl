6e278d9e4c221e8a82657dc14243c33807ce568f
Tim Licata <tlicata@hotpads.com>
2012-03-03 11:05:38 -0800

    my personal site

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tl "0.1.0-SNAPSHOT"
-  :description "My personal site written in Clojure"
+  :description "My personal site"
   :dependencies [[clj-redis "0.0.12"]
                  [noir "1.2.2"]
                  [org.clojure/clojure "1.3.0"]
