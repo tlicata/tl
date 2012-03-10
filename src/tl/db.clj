e68af11802963f86b61c660f5894112ee49c99f5
Tim Licata <tlicata@hotpads.com>
2012-03-10 01:05:13 -0800

    db can work with usernames and bcrypted passwords

--- src/tl/db.clj
+++ src/tl/db.clj
@@ -1,19 +1,30 @@
 (ns tl.db
-  (:require [clj-redis.client :as redis]))
+  (:require [clj-redis.client :as redis]
+            [noir.util.crypt :as crypt]))
 
 (def db-url (System/getenv "REDISTOGO_URL"))
 (def db (redis/init (when db-url {:url db-url})))
 
-(defn add [key val]
-  (try (redis/set db key val)
-       (catch Exception e nil)))
-(defn delete [key]
-  (try (redis/del db key)
-       (catch Exception e nil)))
-(defn retrieve [key]
-  (try (redis/get db key)
+(defn user-exists? [username]
+  (redis/sismember db "users" username))
+
+(defn add-user [username pass]
+  (if (user-exists? username)
+    (throw (Exception. "User already exists"))
+    (do
+      ;; tranaction?
+      (redis/sadd db "users" username)
+      (redis/set db (str username ":hash") (crypt/encrypt pass))))))
+(defn delete-user [username]
+  (tryppp
+    (do
+      (redis/srem db "users" username)
+      (redis/del db [(str username ":hash")]))
+    (catch Exception e nil)))
+(defn get-stored-pass [username]
+  (try (redis/get db (str username ":hash"))
        (catch Exception e "unreachable")))
-(defn get-all-keys []
-  (try (redis/keys db)
+(defn get-all-users []
+  (try (redis/smembers db "users")
        (catch Exception e ["database"])))
 
