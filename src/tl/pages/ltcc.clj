a8bb27a875374411c065d7c0af33f3bd8c0e0af2
Tim Licata <tlicata@hotpads.com>
2012-03-10 19:39:18 -0800

    strip test user code out of ltcc

--- src/tl/pages/ltcc.clj
+++ src/tl/pages/ltcc.clj
@@ -1,43 +1,9 @@
 (ns tl.pages.ltcc
-  (:use [hiccup.core :only [escape-html]]
-        [hiccup.page-helpers :only [link-to]]
-        [noir.core :only [defpage]]
-        [ring.util.response :only [redirect]]
-        [tl.pages.global :only [pagify]])
-  (:require [com.reasonr.scriptjure :as script]
-            [hiccup.form-helpers :as form]
-            [tl.user :as user]))
-
-(defn get-form-for-add []
-  (form/form-to [:post "/admin/ltcc/"]
-                (form/text-field :foo)
-                (form/text-field :bar)
-                (form/submit-button "submit")))
-(defn get-form-for-delete [key]
-  (form/form-to [:delete "/admin/ltcc/"]
-                (form/hidden-field :foo key)
-                (form/submit-button "delete")))
-
-(defn get-row-by-key [key]
-  (let [safe (escape-html key)
-        delete (get-form-for-delete key)]
-    [:tr [:td safe] [:td "value"] [:td delete]]))
+  (:use [noir.core :only [defpage]]
+        [tl.pages.global :only [pagify]]))
 
 (defpage "/ltcc/" []
-  (let [keys (user/all)
-        rows (map get-row-by-key keys)]
-    (pagify
-     {:title ["Ltcc"]
-      :body [[:div (get-form-for-add)]
-             [:div#ltcc (vec (concat [:table] rows))]]
-      :js ["/js/bin/all.min.js"]})))
-
-(defpage [:post "/admin/ltcc/"] {:keys [foo bar]}
-  (do
-    (user/add foo bar)
-    (redirect "/ltcc/")))
-
-(defpage [:delete "/admin/ltcc/"] {:keys [foo]}
-  (do
-    (user/delete foo)
-    (redirect "/ltcc/")))
+  (pagify
+   {:title ["Ltcc"]
+    :body [[:p "Ltcc"]]
+    :js ["/js/bin/all.min.js"]}))
