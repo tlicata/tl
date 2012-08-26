8bcaa8880ff249c45eb38d330c6b0f2f460fc852
Tim Licata <tim@timothylicata.com>
2012-08-25 21:29:41 -0700

    deprecation of lein-swank, long live nrepl.el

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
                  [noir "1.2.2"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]]
-  :plugins [[lein-swank "1.4.3"]]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[ring-mock "0.1.1"]]}}
   :aot [tl.core])
