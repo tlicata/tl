ce0fb8257cb14c8482c802dfea5aa25d829aa1be
Tim Licata <tim@timothylicata.com>
2012-09-16 20:50:33 -0700

    update to clojure 1.4 from 1.3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "My personal site"
   :dependencies [[clj-redis "0.0.12"]
                  [noir "1.3.0-beta10"]
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.1"]]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[ring-mock "0.1.1"]]}}
