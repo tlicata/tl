29ff18ad7a00c45627aab8c0e53aefdb87644ca8
Tim Licata <tim@timothylicata.com>
2012-09-18 21:43:38 -0700

    add hook for building cljs

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [noir "1.3.0-beta10"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.1"]]
+  :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[ring-mock "0.1.1"]]}}
   :plugins [[lein-cljsbuild "0.2.7"]]
