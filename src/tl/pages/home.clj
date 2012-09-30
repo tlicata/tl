884205f353fd8b722869b86a9ddfbd1bc8386c8e
Tim Licata <tim@timothylicata.com>
2012-09-30 11:38:09 -0700

    CLOSURE_NO_DEPS prevents trying to load deps.js

--- src/tl/pages/home.clj
+++ src/tl/pages/home.clj
@@ -2,7 +2,7 @@
   (:use [clojure.data.json :only [read-json]]
         [clojure.string :only [join]]
         [clojure.pprint :only [pprint]]
-        [hiccup.element :only [link-to]]
+        [hiccup.element :only [link-to javascript-tag]]
         [hiccup.page :only [include-js]]
         [ring.util.response :only [redirect]]
         [tl.pages.global :only [pagify]])
@@ -13,9 +13,14 @@
            :body [[:div#particles]]
            :js #{"/js/lib/jgestures.js" "/js/lib/three.js" "/js/particles.js?2"}}))
 
+(defn- include-clojurescript [path]
+  (list
+   (javascript-tag "var CLOSURE_NO_DEPS = true;")
+   (include-js path)))
+
 (defn cljs []
   (pagify {:title ["Cljs Trial"]
            :body [[:div
                    [:input#lname {:type "text" :placeholder "place holder"}]
                    [:input#search-btn {:type "button" :value "Get Stats!"}]]
-                  [:script {:type "text/javascript" :src "/js/bin/all.js"}]]}))
+                  (include-clojurescript "/js/bin/all.js")]}))
