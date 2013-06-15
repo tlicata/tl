2567046a588770315087eac78fcef18b5d4445a0
Tim Licata <tim@timothylicata.com>
2013-06-15 11:44:48 -0700

    stop including bootstrap in pages

--- src/tl/pages/global.clj
+++ src/tl/pages/global.clj
@@ -5,18 +5,15 @@
         [hiccup.page :only [doctype html5 include-css include-js]]))
 
 (def analytics "/js/analytics.js")
-(def bootstrap-css "/css/lib/themes/cyborg/bootstrap.min.css")
-(def bootstrap-css-responsive "/css/lib/bootstrap-responsive.css")
-(def bootstrap-js "/js/lib/bootstrap.js")
 (def jquery "/js/lib/jquery-1.7.1.js")
 (def main "/css/main.css?2")
 
 (defn css [& more]
-  (let [global [bootstrap-css main]]
+  (let [global [main]]
     (apply include-css (concat global more))))
 
 (defn js [& more]
-  (let [global [jquery bootstrap-js]]
+  (let [global [jquery]]
     (apply include-js (concat global more [analytics]))))
 
 (defn blurb [html]
