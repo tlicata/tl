1428f608e024041778ead263c5b1d64fcaf1a611
Tim Licata <tim@timothylicata.com>
2013-10-23 22:05:33 -0700

    remove domina dependency

--- cljs/try.cljs
+++ cljs/try.cljs
@@ -1,8 +1,4 @@
 (ns tl.try
-  (:require [domina :as d]
-            [clojure.browser.event :as event]))
+  (:require [clojure.browser.event :as event]))
 
-(def search-button (d/by-id "search-btn"))
-
-(event/listen search-button "click"
-              (fn [] (js/alert (d/value (d/by-id "lname")))))
+(js/alert "pizza")
