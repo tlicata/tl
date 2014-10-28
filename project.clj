49792105aec84150101e0b2b10ea8a28cc4e61bc
Tim Licata <tim@timothylicata.com>
2014-10-27 23:13:47 -0700

    add ring and hiccup dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject tl "0.1.0-SNAPSHOT"
   :description "My personal site"
   :dependencies [[compojure "1.2.1"]
+                 [hiccup "1.0.5"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/data.json "0.2.5"]
-                 [prismatic/dommy "0.1.1"]]
+                 [prismatic/dommy "0.1.1"]
+                 [ring "1.3.1"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[ring-mock "0.1.1"]]
