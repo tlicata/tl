07972772470f21562bb25384eb40d4a65993f3e0
Tim Licata <tim@timothylicata.com>
2014-10-27 23:11:45 -0700

    upgrade most deps to latest versions

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
 (defproject tl "0.1.0-SNAPSHOT"
   :description "My personal site"
   :dependencies [[clj-redis "0.0.12"]
-                 [compojure "1.1.3"]
-                 [lib-noir "0.2.0"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1933"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
-                 [org.clojure/data.json "0.2.3"]
+                 [compojure "1.2.1"]
+                 [lib-noir "0.9.4"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/data.json "0.2.5"]
                  [prismatic/dommy "0.1.1"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
   :profiles {:dev {:dependencies [[ring-mock "0.1.1"]]
                    :plugins [[com.cemerick/austin "0.1.1"]]}
              :production {:offline true}}
-  :plugins [[lein-cljsbuild "0.3.4"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :aot [tl.core]
   :cljsbuild {:builds [{:source-paths ["cljs"]
                         :id "dev"
