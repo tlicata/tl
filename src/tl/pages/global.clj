a00e3cf47c5fc3cd15640cfa4542892a8a6478f7
Tim Licata <tim@timothylicata.com>
2014-11-16 17:56:48 -0800

    prevent user scaling

--- src/tl/pages/global.clj
+++ src/tl/pages/global.clj
@@ -37,7 +37,7 @@
   `[:head
     [:title ~(join " - " (cons "Tim's Online World" title))]
     [:meta {:charset "UTF-8"}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=yes"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
     ~@(apply css css-arg)
     ~@(apply js js-arg)])
 
