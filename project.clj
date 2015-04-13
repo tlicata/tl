2037602e731ee45a3b4ea90057c56594b561ae91
Tim Licata <tim@timothylicata.com>
2015-04-13 11:31:54 -0700

    avoid compiler warnings from markdown-clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[com.taoensso/carmine "2.9.2"]
                  [compojure "1.2.1"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "0.9.58"]
+                 [markdown-clj "0.9.58" :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
