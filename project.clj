9a1e1b4abb91127fbb51eadfcf22f1515b9c229a
Tim Licata <tim@timothylicata.com>
2017-06-11 01:14:40 +0000

    update all dependencies

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 (defproject tl "0.1.0-SNAPSHOT"
   :description "My personal site"
-  :dependencies [[clj-http "1.1.2"]
-                 [com.taoensso/carmine "2.9.2"]
-                 [compojure "1.2.1"]
+  :dependencies [[clj-http "3.6.1"]
+                 [com.taoensso/carmine "2.16.0"]
+                 [compojure "1.6.0"]
                  [hiccup "1.0.5"]
-                 [markdown-clj "0.9.58" :exclusions [org.clojure/clojure]]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/data.json "0.2.5"]
-                 [prismatic/dommy "0.1.1"]
-                 [ring "1.3.1"]
+                 [markdown-clj "0.9.99"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.562"]
+                 [org.clojure/core.async "0.3.443" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/data.json "0.2.6"]
+                 [prismatic/dommy "1.1.0"]
+                 [ring "1.6.0"]
                  [ring/ring-json "0.4.0"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.1.2"
-  :profiles {:dev {:dependencies [[ring-mock "0.1.1"]]}
+  :profiles {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}
              :prod {:offline? true}
              :uberjar {:main tl.core, :aot :all}}
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.1.6"]]
   :source-paths ["src/clj"]
   :aot [tl.core]
   :uberjar-name "tl-standalone.jar"
